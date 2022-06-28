471f5e2f287bce340615f318da737c3f1def60f4
Amina Ahmed <adewuyiamina@gmail.com>
2022-06-28 16:51:10 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,68 @@
+const reviews = [
+	{
+		id: 1,
+		name: 'Beatrice John',
+		job: 'web developer',
+		image:
+			'https://images.pexels.com/photos/733872/pexels-photo-733872.jpeg?cs=srgb&dl=pexels-andrea-piacquadio-733872.jpg&fm=jpg',
+		text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+	},
+	{
+		id: 2,
+		name: 'Maria Mathew',
+		job: 'web designer',
+		image:
+			'https://images.pexels.com/photos/1181519/pexels-photo-1181519.jpeg?cs=srgb&dl=pexels-christina-morillo-1181519.jpg&fm=jpg',
+		text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+	},
+	{
+		id: 3,
+		name: 'Peter Smith',
+		job: 'intern',
+		image:
+			'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?cs=srgb&dl=pexels-simon-robben-614810.jpg&fm=jpg',
+		text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+	},
+	{
+		id: 4,
+		name: 'Mike Love',
+		job: 'the boss',
+		image:
+			'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?cs=srgb&dl=pexels-justin-shaifer-1222271.jpg&fm=jpg',
+		text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+	},
+	{
+		id: 5,
+		name: 'Hannah Nelson',
+		job: 'web developer',
+		image:
+			'https://images.pexels.com/photos/38554/girl-people-landscape-sun-38554.jpeg?cs=srgb&dl=pexels-pixabay-38554.jpg&fm=jpg',
+		text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+	},
+	{
+		id: 6,
+		name: 'Cara Jacob',
+		job: 'web designer',
+		image:
+			'https://images.pexels.com/photos/871495/pexels-photo-871495.jpeg?cs=srgb&dl=pexels-godisable-jacob-871495.jpg&fm=jpg',
+		text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+	},
+	{
+		id: 7,
+		name: 'Smith Doe',
+		job: 'manager',
+		image:
+			'https://images.pexels.com/photos/1516680/pexels-photo-1516680.jpeg?cs=srgb&dl=pexels-nitin-khajotia-1516680.jpg&fm=jpg',
+		text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+	},
+	{
+		id: 8,
+		name: 'Love Mike',
+		job: 'supervisor',
+		image:
+			'https://images.pexels.com/photos/1036623/pexels-photo-1036623.jpeg?cs=srgb&dl=pexels-moose-photos-1036623.jpg&fm=jpg',
+		text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+	},
+];
+
+export default reviews;
